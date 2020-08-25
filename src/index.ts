0deb4f49c62f4b55587d9269f146410c15d9f6c3
Tobias Golbs <golbs@e-spirit.com>
2020-08-25 16:32:12 +0200

    chore(unused files): remove obsolete files

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,3 @@
-import FSXA from "./core/fsxa";
 import { FSXAModuleOptions as _FSXAModuleOptions } from "./module";
 
 export type FSXAModuleOptions = _FSXAModuleOptions;
-
-declare module "@nuxt/types" {
-  interface Context {
-    readonly $fsxa: FSXA;
-  }
-  interface NuxtAppOptions {
-    readonly $fsxa: FSXAModuleOptions;
-  }
-  interface NuxtOptions {
-    fsxa?: Partial<FSXAModuleOptions>;
-  }
-}
-
-declare module "vue/types/vue" {
-  interface Vue {
-    $fsxa: FSXA;
-  }
-}
-
-/**declare module "vue/types/options" {
-  interface ComponentOptions<V extends Vue> {
-    auth?: true | false | "guest";
-  }
-}**/
-
-declare module "vuex/types/index" {
-  interface Store<S> {
-    $fsxa: FSXA;
-  }
-}
