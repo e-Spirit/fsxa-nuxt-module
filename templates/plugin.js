8d0692f870c8cee0dd3c584a7c048a13ca4a9471
Tobias Golbs <golbs@e-spirit.com>
2020-09-08 10:10:06 +0200

    fix(plugin): update client-check
    Switched to undefined check of window to make sure that we are on the server / client

--- templates/plugin.js
+++ templates/plugin.js
@@ -3,9 +3,10 @@ import { getFSXAModule } from "fsxa-pattern-library";
 export default function (ctx, inject) {
   const fsxaModule = getFSXAModule(process.env.FSXA_MODE, {
     mode: "proxy",
-    baseUrl: window
-      ? "/api/fsxa"
-      : `http://localhost:${process.env.NUXT_HOST || 3000}/api/fsxa`,
+    baseUrl:
+      typeof window !== "undefined"
+        ? "/api/fsxa"
+        : `http://localhost:${process.env.NUXT_HOST || 3000}/api/fsxa`,
   });
   if (typeof ctx.store === "undefined") {
     throw new Error(
