c8762e0b23798380232f31089eacbb299aebaa4d
p-runge <46087376+p-runge@users.noreply.github.com>
2021-12-20 16:33:06 +0100

    fix: fix protocol for api calls from localhost (#23)
    Fixed missing protocol bug with error message: "fetch API cannot load (..). URL scheme "localhost" is not supported.".
    Also updated CHANGELOG.md with a small migration guide for PWAs.

--- templates/plugin.js
+++ templates/plugin.js
@@ -7,7 +7,7 @@ export default function (ctx, inject) {
   let url = path
   if (typeof window === "undefined") {
     // server-side rendering
-    url = `${envConfig.FSXA_HOST || "localhost"}:${envConfig.FSXA_PORT || 3000}${url}`
+    url = `${envConfig.FSXA_HOST || "http://localhost"}:${envConfig.FSXA_PORT || 3000}${url}`
   }
 
   const proxyApiConfig = {
